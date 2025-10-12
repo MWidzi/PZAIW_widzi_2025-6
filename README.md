25495da21f3aa1b0d3ba450b4cacbde3fdf31683
Maciej Widzinski <mlydzy123@gmail.com>
2025-10-12 21:48:16 +0200

    projekt na 13-10-2025

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PZAIW_widzi_2025-6
+## Repozytorium do programowania zaawansowanych aplikacji webowych w roku szkolnym 2025/6
+
+### Legenda folderów:
+- projekt na 13-10-2025: first-server
---
+++ first-server/index.js
@@ -0,0 +1,23 @@
+import { createServer } from 'node:http';
+import { URL } from "node:url";
+import { handlePath } from "./src/path_handlers.js";
+
+const server = createServer((req, res) => {
+    const request_url = new URL(`http://${host}${req.url}`);
+    const path = request_url.pathname;
+    console.log(`Request: ${req.method} ${path}`);
+
+    handlePath(path, req, res);
+
+    if (!res.writableEnded) {
+        res.writeHead(404, { 'Content-Type': 'text/plain'});
+        res.end('Site not found!\n');
+    }
+});
+
+const port = 8000;
+const host = "localhost";
+
+server.listen(port, host, () => {
+    console.log(`Server listening on http://${host}:${port}`);
+});
